4cce2113ea68d23f89013627404cfe4ee263b6c6
sjjeong <sjjeong-at-570146075219>
2022-06-13 17:54:08 +0900

    Java Effective Item2 Freezing

---
+++ effective-java/src/main/java/me/jsj/item2/freeze/Person.java
@@ -0,0 +1,30 @@
+package me.jsj.item2.freeze;
+
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@ToString
+public class Person {
+
+    private final String name;
+    private final int birthYear;
+    private final List<String> grade;
+
+    public Person(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+        this.grade = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        final Person person = new Person("jsj", 1995);
+//        person.name = "정세진";
+        log.info(person.toString());
+
+//        person = new Person("세진", 1995);
+    }
+}
