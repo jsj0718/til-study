f983ac462ee83947751ce7b294807a05478f2e80
sjjeong <sjjeong-at-570146075219>
2022-06-13 09:15:54 +0900

    TDD 예제 진행

---
+++ tdd/src/main/java/money/Pair.java
@@ -0,0 +1,25 @@
+package money;
+
+import java.util.Objects;
+
+public class Pair {
+
+    private String from;
+    private String to;
+
+    public Pair(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Pair pair = (Pair) o;
+        return Objects.equals(from, pair.from) && Objects.equals(to, pair.to);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
